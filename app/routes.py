a45b468e28ae49ea9e1f1d968d14f3f70dcc290c
Suresh Panigrahi <suresh.panigrahi@billmart.com>
2025-04-03 11:48:01 +0530

    Image liveness checked by using  OULU_Protocol_2_model_0_0.onnx model

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,31 @@
 # app/routes.py
 
-from flask import Blueprint, jsonify
-from app.services.common_service import get_hello_message
+from flask import Blueprint, jsonify, request
+from app.services.common_service import get_hello_message, face_liveness_check
+from app.services.liveness_service import LivenessDetection
+import numpy as np
+from PIL import Image
 
 api_blueprint = Blueprint('api', __name__)
+
+liveness_checker = LivenessDetection(checkpoint_path="model/OULU_Protocol_2_model_0_0.onnx")
 
 @api_blueprint.route('/api/hello', methods=['GET'])
 def hello():
     response = get_hello_message()
     return jsonify(response);
 
-@api_blueprint.route('/api/liveness', methods = ['GET'])
-def liveness():
-    return jsonify({'status': 'ok'})
+@api_blueprint.route('/api/liveness-check', methods=['POST'])
+def liveness_check():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+
+    file = request.files['image']
+    image = Image.open(file.stream)
+    image = image.convert("RGB")
+    image_np = np.array(image)
+
+    score = liveness_checker(image_np)
+    result = "Live" if score > 0.5 else "Spoof"
+
+    return jsonify({"result": result, "score": score})
--- app/services/common_service.py
+++ app/services/common_service.py
@@ -1,2 +1,5 @@
 def get_hello_message():
     return {"message": "Hello, Flask API with dotenv and separated business logic!"}
+
+def face_liveness_check():
+    return {"message": "Face LiveNess Check"}
